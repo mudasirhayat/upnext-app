9ce484e97624e884e1ffe64b9f748a4a870dd2da
mudasirhayat <mudasirhayat5@gmail.com>
2025-08-14 05:00:01 +0000

    fix: improve error handling

--- duett_api/patients/migrations/0033_historicalpatient_historicalpatientrequest_historicalservicerequested.py
+++ duett_api/patients/migrations/0033_historicalpatient_historicalpatientrequest_historicalservicerequested.py
@@ -58,8 +58,8 @@ class Migration(migrations.Migration):
                 ('requested_schedule', models.CharField(blank=True, max_length=255, null=True)),
                 ('history_id', models.AutoField(primary_key=True, serialize=False)),
                 ('history_date', models.DateTimeField()),
-                ('history_change_reason', models.CharField(max_length=100, null=True)),
-                ('history_type', models.CharField(choices=[('+', 'Created'), ('~', 'Changed'), ('-', 'Deleted')], max_length=1)),
+('history_change_reason', models.CharField(max_length=100, null=True)),
+                ('history_type', models.CharField(choices=[('+', 'Created'), ('~', 'Changed'), ('-', 'Deleted')], max_length=1, error_messages={'max
                 ('created_by', models.ForeignKey(blank=True, db_constraint=False, null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to=settings.AUTH_USER_MODEL)),
                 ('history_user', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to=settings.AUTH_USER_MODEL)),
                 ('patient', models.ForeignKey(blank=True, db_constraint=False, null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='+', to='patients.patient')),
