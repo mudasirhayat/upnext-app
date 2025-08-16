0a23cf3c86db39385682ac0cb5a5226d84f39cbc
mudasirhayat <mudasirhayat5@gmail.com>
2025-08-16 17:00:01 +0000

    fix: improve error handling

--- upnext-react/src/screens/signup.js
+++ upnext-react/src/screens/signup.js
@@ -1,8 +1,11 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
+try {
+  import CssBaseline from "@mui/material/CssBaseline";
+  import TextField from "@mui/material/TextField";
+  import FormControlLabel from "@mui/material/FormControlLabel";
+} catch (error) {
+  console.error("Error
 import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
