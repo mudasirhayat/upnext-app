6c82210766668694b8d76d69c92ddf7709efb33f
mudasirhayat <mudasirhayat5@gmail.com>
2025-08-23 05:00:01 +0000

    fix: improve error handling

--- upnext-react/src/App.test.js
+++ upnext-react/src/App.test.js
@@ -2,6 +2,9 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement
 render(<App />);
 const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
