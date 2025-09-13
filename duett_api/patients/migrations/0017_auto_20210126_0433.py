ac6235da69e49e11eab75e580cbe123898442b06
mudasirhayat <mudasirhayat5@gmail.com>
2025-09-13 17:00:02 +0000

    refactor: improve code readability

--- duett_api/patients/migrations/0017_auto_20210126_0433.py
+++ duett_api/patients/migrations/0017_auto_20210126_0433.py
@@ -11,8 +11,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name="patientrequest",
+migrations.AddField(
+    model_name="patientrequest",
             name="hides",
             field=models.ManyToManyField(
                 blank=True,
