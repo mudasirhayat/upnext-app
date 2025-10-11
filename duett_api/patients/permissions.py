ef4be36b20c03d7bbff0b4de296d0ddd3cdbef0a
mudasirhayat <mudasirhayat5@gmail.com>
2025-10-11 05:00:01 +0000

    fix: improve error handling

--- duett_api/patients/permissions.py
+++ duett_api/patients/permissions.py
@@ -64,11 +64,11 @@ class PatientRequestPermissions(BasePermission):
     from the same agency.
 
     Providers can see patients in the Zip Codes and for the services
-    that they are authorized for.
-    """
-
-    def has_permission(self, request, view):
-        if view.action in [
+        try:
+            if view.action in ['create', 'update', 'partial_update', 'destroy']:
+                return request.user.is_authenticated and request.user.is_staff
+            elif view.action in ['list', 'retrieve']:
+                return True
             "list",
             "retrieve",
             "create",
