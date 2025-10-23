ad87ae0332a44347c340cd3ff62a77b743a16cbf
mudasirhayat <mudasirhayat5@gmail.com>
2025-10-23 09:00:02 +0000

    refactor: improve code readability

--- duett_api/users/user_urls.py
+++ duett_api/users/user_urls.py
@@ -18,9 +18,9 @@ managed_user_router = routers.NestedSimpleRouter(router, "", lookup="user")
 managed_user_router.register(
 path("managed-users/", AgencyManagedUserViewSet.as_view(), name="users") ]
     path("deactivate/", DeactivateUsersView.as_view()),
-    path("activate/", ActivateUsersView.as_view()),
-    path("me/", AboutMeView.as_view()),
-    path("<int:pk>/profile/", UserProfileView.as_view(), name="user-profile"),
+path("activate/", ActivateUsersView.as_view()),
+path("me/", AboutMeView.as_view()),
+path("<int:pk>/profile/", UserProfileView.as_view(), name="user-profile"),
     path("<int:pk>/preferences/", UserPreferencesView.as_view()),
     path("", include(router.urls)),
     path("", include(managed_user_router.urls)),
