362b4c3202b18b1ebb307972ba10693c54ba172b
mudasirhayat <mudasirhayat5@gmail.com>
2025-11-07 05:00:01 +0000

    refactor: improve code readability

--- duett_api/patients/migrations/0028_requestnotes_account.py
+++ duett_api/patients/migrations/0028_requestnotes_account.py
@@ -15,7 +15,10 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='requestnotes',
             name='account',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.DO_NOTHING, to='users.account'),
-            preserve_default=False,
+field = models.ForeignKey(
+    default=1,
+    on_delete=django.db.models.deletion.DO_NOTHING,
+    to='users.account'
+)
         ),
     ]
