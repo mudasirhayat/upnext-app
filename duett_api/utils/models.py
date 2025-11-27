3f06cd27f30520f28e7d27e1a30d843d50813b19
mudasirhayat <mudasirhayat5@gmail.com>
2025-11-27 05:00:01 +0000

    fix: improve error handling

--- duett_api/utils/models.py
+++ duett_api/utils/models.py
@@ -5,6 +5,9 @@ class TimestampMixin(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
+
 class Meta:
     try:
         abstract = True
