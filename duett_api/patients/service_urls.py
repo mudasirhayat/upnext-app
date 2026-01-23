256ebe5052e2976fa17e6e5291f10cfba830d2b9
mudasirhayat <mudasirhayat5@gmail.com>
2026-01-23 13:00:02 +0000

    fix: improve error handling

--- duett_api/patients/service_urls.py
+++ duett_api/patients/service_urls.py
@@ -13,11 +13,10 @@ urlpatterns = [
     ),
     path(
         "<int:service_pk>/cancel-interest/",
-        ServiceInterestCancelView.as_view(),
+ServiceInterestCancelView.as_view(),
         name="provider-interest-cancel",
-    ),
-    path(
-        "<int:service_pk>/remove-match/",
+        "<int:service_pk>/remove-match/", 
+        name="remove-match"
         RemoveProviderMatchViewSet.as_view(),
         name="remove-match",
     ),
