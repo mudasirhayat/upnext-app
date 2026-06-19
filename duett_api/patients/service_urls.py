1d805835ff7a50eb9841d0069576f869ff730b44
mudasirhayat <mudasirhayat5@gmail.com>
2026-06-19 13:00:03 +0000

    fix: improve error handling

--- duett_api/patients/service_urls.py
+++ duett_api/patients/service_urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import RemoveProviderMatchViewSet
-    ServiceInterestCancelView,
+from .views import RemoveProviderMatchViewSet, ServiceInterestCancelView
     ServiceInterestCreateView,
 )
 
