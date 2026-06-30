663111caeadc3b14aadb5ad5b076e3a67bfd5897
mudasirhayat <mudasirhayat5@gmail.com>
2026-06-30 09:00:03 +0000

    refactor: improve code readability

--- duett_api/patients/migrations/0022_servicerequested_requested_schedule.py
+++ duett_api/patients/migrations/0022_servicerequested_requested_schedule.py
@@ -4,9 +4,9 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("patients", "0021_auto_20210209_2212"),
+    ]
     ]
 
     operations = [
